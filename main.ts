3155c00b929738867224a7c0681a63ae9e048143
PrashantCreator <140830590+PrashantCreator@users.noreply.github.com>
2023-07-28 20:09:56 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,3 @@
+scene.setBackgroundImage(assets.image`BG`)
+let mySprite = sprites.create(assets.image`Axol`, SpriteKind.Player)
+music.play(music.melodyPlayable(music.baDing), music.PlaybackMode.UntilDone)
